9baaa87dd752067c107f4835599744393e60cf1f
Dekevion Wade <Dekevionwade@yahoo.com>
2019-09-20 16:14:19 -0500

    answers

--- question7.py
+++ question7.py
@@ -2,7 +2,7 @@
 # Create the class Books with name, rating, genre, and author properties/attributes. Create a class method that will change the rating of the book. Outside of the class, create three objects of the class Book and put them in an array. Lastly, USING THE ARRAY print only the names of the books using any method we’ve learned in class.
 
 class Books:
-    def _init_(self, name, rating, genre, author):
+    def __init__(self, name, rating, genre, author):
         self.name = name
         self.rating = rating
         self.genre = genre
@@ -11,11 +11,16 @@ class Books:
     def changeRating(self, newRating):
         self.rating = newRating
 
-book1 = Books("Programmng Python", 10, "Non-Fiction", "Kenn")
-book2 = Books("Programmng JavaScript", 10, "Non-Fiction", "Kevin")
-book3 = Books("Programmng HTML", 10, "Non-Fiction", "Erin")
+
+
+book1 = Books('abc','wd','aca','cs')
+book2 = Books('abc','wd','aca','cs')
+book3 = Books('abc','wd','aca','cs')
 
 bookArray = [book1, book2, book3]
 
-for eachBook in bookArray:
-    print(eachBook.name())
+print(book1.name)
+
+print(book2.name)
+
+print(book3.name)
